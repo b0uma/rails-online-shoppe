9f2bc58013de562a4f4c1ea87d37f39aedd36df1
Julian Bouma <bouma@post.harvard.edu>
2016-02-20 12:42:52 -0800

    users controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:valid_attributes) do
+    {name: 'Big J', email: 'biggie@devbootcamp.com', password: 'open sesame'}
+  end
+
+  let(:invalid_attributes) do
+    {name: '', email: '', password: ''}
+  end
+
+  describe "GET signup" do
+    it "assigns a new product as @product" do
+      get :new, {}
+      expect(assigns(:user)).to be_a_new(User)
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid params" do
+      it "creates a new User" do
+        expect {
+          post :create, {:user => valid_attributes}
+          }.to change(User, :count).by(1)
+      end
+
+      it "assigns a newly created user as @user" do
+        post :create, {:user => valid_attributes}
+        expect(assigns(:user)).to be_a(User)
+        expect(assigns(:user)).to be_persisted
+      end
+
+      it "redirects to the products page after signup" do
+        post :create, {:user => valid_attributes}
+        expect(response).to redirect_to "/products"
+      end
+    end
+
+    context "with invalid params" do
+      it "assigns a newly created but unsaved user as @user" do
+        post :create, {:user => invalid_attributes}
+        expect(assigns(:user)).to be_a_new(User)
+      end
+
+      it "re-renders the 'new' template" do
+        post :create, {:user => invalid_attributes}
+        expect(response).to render_template("new")
+      end
+    end
+  end
+
+   describe "Routing" do
+    it "/signup routes to users#new" do
+      expect(:get => "/signup").to route_to("users#new")
+    end
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid) do
+    User.new({name: 'Big J', email: 'biggie@devbootcamp.com', password: 'open sesame'})
+  end
+
+  let(:invalid) do
+    User.new({name: '', email: '', password: ''})
+  end
+
+  describe "user validations" do
+    context "will create an error" do
+      it "when fields are empty" do
+        invalid.save
+        expect(invalid.errors[:name]).to include("can't be blank")
+        expect(invalid.errors[:email]).to include("can't be blank")
+        expect(invalid.errors[:password_digest]).to include("can't be blank")
+      end
+    end
+    it 'saves with valid data' do
+      expect{valid.save}.to change{User.count}.by(1)
+    end
+  end
 end
