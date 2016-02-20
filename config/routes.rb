d209feb5d0731cd6862ad33b02359d2897a45199
Julian Bouma <bouma@post.harvard.edu>
2016-02-19 20:31:56 -0800

    routes, controllers, and spec files created for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'products#index'
   resources :products
   get '/admin', to: 'products#admin'
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
 end
