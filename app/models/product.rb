76fd5c499089c80b3e9ce55767af52de69107390
Julian Bouma <bouma@post.harvard.edu>
2016-02-20 18:27:13 -0800

    associations update and join table migration implemented

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+  has_and_belongs_to_many :categories
+
   validates :name, :price, :description, :image_url, :quantity, presence: true
   validates_numericality_of :price, greater_than: 0
   validates_numericality_of :quantity, greater_than: 0
--- db/migrate/20160221022307_create_categories.rb
+++ db/migrate/20160221022307_create_categories.rb
@@ -1,8 +1,13 @@
 class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
-
+      t.string :name
       t.timestamps null: false
+    end
+
+    create_join_table :products, :categories do |t|
+      t.index :product_id
+      t.index :category_id
     end
   end
 end
