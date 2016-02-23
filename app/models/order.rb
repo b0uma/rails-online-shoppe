af540cd4af7c6096ccd1f9c4c7a8e6bc7d0dc738
Julian Bouma <bouma@post.harvard.edu>
2016-02-23 12:07:16 -0800

    has_many associations for orders and users

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ActiveRecord::Base
   belongs_to :user
+  has_many :order_items, dependent: :destroy
 
   enum status: { pending: 0, complete: 1 }
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :name, :email, :password_digest, presence: true
+
+  has_many :orders
 end
