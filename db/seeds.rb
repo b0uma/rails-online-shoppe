036ecf3523aaca01393ff61093ad667b6e04efb3
Julian Bouma <bouma@post.harvard.edu>
2016-02-23 12:36:12 -0800

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Product.destroy_all
 Category.destroy_all
+Order.destroy_all
+
+sample_user = User.first
 
 Category.create(name: 'Food')
 Category.create(name: 'Electronics')
@@ -23,3 +26,21 @@ Category.create(name: 'Workout')
   product.categories = Category.limit(2).order("RANDOM()")
   product.save
 end
+
+# out of stock item
+Product.create({"id"=>21,
+ "name"=>"Black Lotus",
+ "price"=>999.0,
+ "description"=>"very very rare, believe me",
+ "image_url"=>
+  "http://orig00.deviantart.net/58e2/f/2008/218/d/a/black_lotus_mtg_alter_proxy_by_michaelnoel.jpg",
+ "quantity"=>0})
+
+sample_user.orders.create(status: Orders.statuses[:pending], checkout_time: nil)
+order = sample_user.orders.first
+prod1 = Product.first
+prod2 = Product.last
+order.order_items.create(product_id: prod1.id, quantity: prod1.quantity)
+order.order_items.create(product_id: prod2.id, quantity: prod2.quantity)
+
+
