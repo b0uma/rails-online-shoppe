ba96874ada48954e984bebbf3cb139a41fb94ecc
Julian Bouma <bouma@post.harvard.edu>
2016-02-23 10:32:58 -0800

    proper enum syntax

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ActiveRecord::Base
   belongs_to :user
 
-  enum { pending: 0, complete: 1 }
+  enum status: { pending: 0, complete: 1 }
 
   validates :checkout_time, :status, presence: true
 end
