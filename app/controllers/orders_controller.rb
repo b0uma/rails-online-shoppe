e9a661c1872d05e2e8ee8613090b61e31c4b63ba
Julian Bouma <bouma@post.harvard.edu>
2016-02-23 16:46:27 -0800

    new routes for checkout, process order logic

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,12 +10,23 @@ class OrdersController < ApplicationController
   end
 
   def index
+    @orders = current_user.orders
   end
 
   def checkout
+    @order_to_close = current_user.orders.find(params[:id])
+    if @order_to_close.nil?
+      flash[:danger] = 'Order not found'
+      redirect_to '/cart'
+    else
+      @order_to_close.status = Order.statuses[:complete]
+      @order_to_close.save
+      render 'checkout'
+    end
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
   root 'products#index'
   resources :products
+
   get '/admin', to: 'products#admin'
+
   get '/signup', to: 'users#new'
   post '/users', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
   post '/change_categories/:product_id', to: 'products#update_categories'
+
   get '/cart', to: 'orders#cart'
   post '/cart', to: 'orders#add_to_cart'
+
+  get '/orders', to: 'orders#index'
+  post '/orders/:id', to: 'orders#checkout'
+  get '/orders/:id', to 'orders#show'
 end
