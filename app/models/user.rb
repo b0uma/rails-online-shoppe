62980d6e70acee70c41311482aa3de0489b9c3c6
Julian Bouma <bouma@post.harvard.edu>
2016-04-20 11:50:23 -0700

    delete orders when user is deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :name, :email, :password_digest, presence: true
 
-  has_many :orders
+  has_many :orders, dependent: :destroy
 
   after_create :create_initial_order
 
