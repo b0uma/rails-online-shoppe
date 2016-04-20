9cec90c28cf321f77090b014f7fa7b34a7083a07
Julian Bouma <bouma@post.harvard.edu>
2016-04-20 12:50:22 -0700

    password confirmation logic in registration added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new(user_params.except(:password_confirm))
     if @user.save
       flash[:success] = "User account successfully created!"
       login(@user)
       redirect_to "/products"
+    elsif user_params[:password] != user_params[:password_confirm]
+      flash.now[:danger] = "Your passwords did not match"
+      render :new
     else
       flash.now[:danger] = @user.errors.full_messages
       render :new
@@ -18,6 +21,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    params.require(:user).permit(:name, :email, :password, :password_confirm)
   end
 end
